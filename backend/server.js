bc1e184cde122639a7078cc9ea52abf8d4dd4894
syeed111 <syeedimtiaz111@gmail.com>
2025-05-16 09:22:49 +0600

    controllers added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -18,6 +19,22 @@ app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+async function initDB() {
+  try {
+    await sql` 
+    CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP )`;
+  } catch (error) {
+    console.log("Error in initDB()", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
 
 //postgresql://neondb_owner:npg_ajD1lqcg4Kpd@ep-late-meadow-a8g9ej4r-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
